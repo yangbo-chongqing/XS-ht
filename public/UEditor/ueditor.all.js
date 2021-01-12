8754f7fe120a155e0e73b0b71838b3a27b8e9ba8
w <z08w13@gmail.com>
2021-01-12 14:19:31 +0800

    更改点击按钮同时出发下拉

--- public/UEditor/ueditor.all.js
+++ public/UEditor/ueditor.all.js
@@ -29245,11 +29245,12 @@
       },
       getHtmlTpl: function () {
         return (
+          //点击按钮吧_onButtonClick更换为_onArrowClick
           '<div id="##" class="edui-box %%">' +
           "<div " +
           (this.title ? 'title="' + this.title + '"' : "") +
           ' id="##_state" stateful><div class="%%-body">' +
-          '<div id="##_button_body" class="edui-box edui-button-body" onclick="$$._onButtonClick(event, this);">' +
+          '<div id="##_button_body" class="edui-box edui-button-body" onclick="$$._onArrowClick(event, this);">' +
           '<div class="edui-box edui-icon"></div>' +
           "</div>" +
           '<div class="edui-box edui-splitborder"></div>' +
